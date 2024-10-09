fe0e7de449df71455d16e963cb7a7081a91a01bd
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-09 17:08:09 -0400

    Add footer and finish basic header layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/images/logo.svg";
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
 
 const Header = () => {
     return (
@@ -8,30 +9,30 @@ const Header = () => {
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center">
-            <a href="/">
+            <Link href="/">
               <Image className="h-12 w-12" src={ logo } alt="Bookit" priority={true}/>
-            </a>
+            </Link>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                <a
+                <Link
                   href="/"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Rooms
-                </a>
+                </Link>
                 {/* <!-- Logged In Only --> */}
-                <a
+                <Link
                   href="/bookings.html"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Bookings
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/add-room.html"
                   className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Add Room
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -39,27 +40,27 @@ const Header = () => {
           <div className="ml-auto">
             <div className="ml-4 flex items-center md:ml-6">
               {/* <!-- Logged Out Only --> */}
-              <a
+              <Link
                 href="login.html"
                 className="mr-3 text-gray-800 hover:text-gray-600"
               >
-                <i className="fa fa-sign-in"></i> Login
-              </a>
-              <a
+                <FaSignInAlt className="inline mr-1"/> Login
+              </Link>
+              <Link
                 href="register.html"
                 className="mr-3 text-gray-800 hover:text-gray-600"
               >
-                <i className="fa fa-user"></i> Register
-              </a>
-              <a href="my-rooms.html">
-                <i className="fa fa-building"></i> My Rooms
-              </a>
-              <a
+                <FaUser className="inline mr-1"/> Register
+              </Link>
+              <Link href="my-rooms.html">
+                <FaBuilding className="inline mr-1"/> My Rooms
+              </Link>
+              <Link
                 href="login.html"
                 className="mx-3 text-gray-800 hover:text-gray-600"
               >
-                <i className="fa fa-sign-out"></i> Sign Out
-              </a>
+                <FaSignOutAlt className="inline mr-1"/> Sign Out
+              </Link>
             </div>
           </div>
         </div>
@@ -68,25 +69,25 @@ const Header = () => {
       {/* <!-- Mobile menu --> */}
       <div className="md:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <a
+          <Link
             href="/"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Rooms
-          </a>
+          </Link>
           {/* <!-- Logged In Only --> */}
-          <a
+          <Link
             href="/bookings.html"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Bookings
-          </a>
-          <a
+          </Link>
+          <Link
             href="/add-room.html"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Add Room
-          </a>
+          </Link>
         </div>
       </div>
     </header>
