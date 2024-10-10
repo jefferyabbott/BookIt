6d2f1f9a357a2cdc674fd7ff794b02d24ba5cc0f
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-10 07:28:33 -0400

    implement authentication

---
+++ app/actions/destroySession.js
@@ -0,0 +1,27 @@
+'use server';
+import { createSessionClient } from '@/config/appwrite';
+import { cookies } from 'next/headers';
+
+async function destroySession() {
+  
+  // retrieve session cookie
+  const sessionCookie = cookies().get('appwrite-session');
+
+  if (!sessionCookie) {
+    return {error: 'No session cookie found'};
+  }
+
+  try {
+    const { account } = await createSessionClient(sessionCookie.value);
+    // delete session 
+    await account.deleteSession('current');
+    // clear session cookie
+    cookies().delete('appwrite-session');
+    return {success: true};
+  } catch (error) {
+    return {error: 'Error deleting session'};
+  }
+
+}
+
+export default destroySession;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,24 @@
+'use client';
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/images/logo.svg";
 import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
+import destroySession from "@/app/actions/destroySession";
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        const { success, error } = await destroySession();
+        if (success) {
+            router.push('/login');
+        } else {
+            toast.error(error);
+        }
+    };
+
     return (
         <header className="bg-gray-100">
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -55,12 +70,9 @@ const Header = () => {
               <Link href="my-rooms.html">
                 <FaBuilding className="inline mr-1"/> My Rooms
               </Link>
-              <Link
-                href="/login"
-                className="mx-3 text-gray-800 hover:text-gray-600"
-              >
+              <button onClick={handleLogout} className="mx-3 text-gray-800 hover:text-gray-600">
                 <FaSignOutAlt className="inline mr-1"/> Sign Out
-              </Link>
+              </button>
             </div>
           </div>
         </div>
