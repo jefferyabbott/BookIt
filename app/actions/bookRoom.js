534563521cce53beb9555a63e6566c75f044da44
Jeffery Abbott <jeff@jefferyabbott.com>
2024-10-12 09:04:26 -0400

    Add check for room availability

--- app/actions/bookRoom.js
+++ app/actions/bookRoom.js
@@ -6,6 +6,7 @@ import { cookies } from 'next/headers';
 import { ID } from 'node-appwrite';
 import checkAuth from "./checkAuth";
 import { revalidatePath } from "next/cache";
+import checkRoomAvailability from "./checkRoomAvailability";
 
 async function bookRoom(previousState, formData) {
   const sessionCookie = cookies().get('appwrite-session');
@@ -29,15 +30,25 @@ async function bookRoom(previousState, formData) {
     const checkInTime = formData.get('check_in_time');
     const checkOutDate = formData.get('check_out_date');
     const checkOutTime = formData.get('check_out_time');
+    const roomId = formData.get('room_id');
 
     const checkInDateTime = `${checkInDate}T${checkInTime}`;
     const checkOutDateTime = `${checkOutDate}T${checkOutTime}`;
+
+    // check if room is available
+    const isAvailable = await checkRoomAvailability(roomId, checkInDateTime, checkOutDateTime);
+
+    if (!isAvailable) {
+      return {
+        error: 'This room is already booked during that time.'
+      }
+    }
 
     const bookingData = {
       check_in: checkInDateTime,
       check_out: checkOutDateTime,
       user_id: user.id,
-      room_id: formData.get('room_id')
+      room_id: roomId
     }
 
     // create booking
